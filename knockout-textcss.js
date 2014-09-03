22b5efdcc001c7d944324262c3cccbb92dbd9b6e
Alexander Bihary <alex.bihary@gmail.com>
2014-09-03 14:30:02 -0400

    add pretty option

--- knockout-textcss.js
+++ knockout-textcss.js
@@ -1,14 +1,12 @@
+// knockout-textcss
 // <styles data-bind="textcss: { selector:'body:hover', styles:{ border:'2px solid red' } }"></styles>
-// <pre data-bind="textcss: { styles:{ border:'2px solid red' } }"></pre>
+// <pre data-bind="textcss: { styles:{ border:'2px solid red', pretty:true } }"></pre>
 
-;(function (factory) {
-    //CommonJS
+; (function (factory) {
     if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
         factory(require("knockout"), exports);
-    //AMD
     } else if (typeof define === "function" && define.amd) {
         define(["knockout", "exports"], factory);
-    //normal script tag
     } else {
         factory(ko, {});
     }
@@ -36,27 +34,33 @@
         return document.documentElement.style[attr] !== undefined && typeof ko.unwrap(value) === 'string';
     }
 
-    function cssRulesFromObject(styles) {
-        var rules = [];
+    function cssRulesFromObject(styles, pretty) {
+        var rules = [],
+            separator = !!pretty ? ': ' : ':',
+            prefix = !!pretty ? '\n  ' : '';
+
         ko.utils.objectForEach(ko.unwrap(styles) || {}, function (attr, value) {
             if (cssRuleIsValid(attr, value))
-                rules.push(attr + ':' + value);
+                var rule = prefix + attr + separator + value;
+                rules.push(rule);
         });
+
         return rules.join(';');
     }
 
     ko.bindingHandlers.textcss = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            // This will be called when the binding is first applied to an element
-            // Set up any initial state, event handlers, etc. here
             ko.computed(function () {
                 var settings = ko.unwrap(valueAccessor()),
+                    pretty = !!ko.unwrap(settings.pretty),
+                    openingBrace = !!pretty ? ' {' : '{',
+                    closingBrace = !!pretty ? '\n}' : '}',
                     selector = settings.id
-                        ? '#' + settings.id 
+                        ? '#' + settings.id
                         : settings.selector || selectorPath(element);
 
                 if (!selector) return;
-                element.innerText = selector + '{' + cssRulesFromObject(settings.styles) + '}';
+                element.innerText = selector + openingBrace + cssRulesFromObject(settings.styles, pretty) + closingBrace;
             });
         }
     };
